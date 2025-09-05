f7d2a19a49b19c810c0088320399df5dd006998d
Boburov Shukurullo <boburovdev@gmail.com>
2025-09-06 03:33:41 +0500

    push

---
+++ server/prisma/migrations/20250904180315_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "surname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "public"."Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
