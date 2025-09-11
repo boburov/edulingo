1168c5ab6db28d1125a518bcbfca081b95172650
silvante <khamidov.ko@gmail.com>
2025-09-11 21:37:06 +0500

    PORT

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { RoleGuard } from 'src/guards/guards.guard';
@@ -6,7 +15,7 @@ import { AuthGuard } from 'src/guards/roles.decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService) {}
 
   @Post()
   create(@Body() createAuthDto: CreateAuthDto) {
@@ -24,5 +33,4 @@ export class AuthController {
   getDashboard() {
     return 'Admin paneli';
   }
-
 }
