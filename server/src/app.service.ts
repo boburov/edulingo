4d499a563eda4a8daf179fad17c2a05b62e3106c
silvante <khamidov.ko@gmail.com>
2025-09-12 17:59:52 +0500

    If to say I found better ways to handle admin seed

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,15 +1,30 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { SeedQueue } from './jobs/processors/admin_seed/seed.queue';
+import { PrismaService } from './prisma/prisma.service';
+import * as bcryptjs from 'bcryptjs';
 
 @Injectable()
 export class AppService implements OnModuleInit {
-  constructor(private readonly seedQueue: SeedQueue) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async onModuleInit() {
     console.log('getting admin password');
 
     const password: string = String(process.env.DEFAULT_ADMIN_PASSWORD);
-    await this.seedQueue.SeedAdmin(password);
+    const existing_admin = await this.prisma.admin.findFirst();
+
+    if (!existing_admin) {
+      const hashed_password = bcryptjs.hashSync(password, 10);
+
+      await this.prisma.admin.create({
+        data: {
+          password: hashed_password,
+        },
+      });
+
+      console.log('New admin seeded');
+    } else {
+      console.log('Admin already present');
+    }
   }
 
   getHello(): string {
