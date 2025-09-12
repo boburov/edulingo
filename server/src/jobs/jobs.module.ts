eb5ff98f7d0363f3cf3d2e3669f249681f927580
silvante <khamidov.ko@gmail.com>
2025-09-12 17:18:08 +0500

    background jobs

---
+++ server/src/jobs/jobs.module.ts
@@ -0,0 +1,27 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { BullModule } from '@nestjs/bullmq';
+import { Module } from '@nestjs/common';
+import { MailQueue } from './processors/mail/mail.queue';
+import { MailProcessor } from './processors/mail/mail.processor';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { QUEUE_NAME } from 'src/constants';
+
+@Module({
+  imports: [
+    MailerModule,
+    BullModule.forRoot({
+      connection: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: QUEUE_NAME,
+    }),
+    PrismaModule,
+  ],
+  providers: [MailQueue, MailProcessor],
+  exports: [MailQueue],
+})
+export class JobsModule {}
+``;
