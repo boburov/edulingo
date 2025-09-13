4053e7bee49098015f4093d24bb79aaf54420136
silvante <khamidov.ko@gmail.com>
2025-09-13 19:34:03 +0500

    enough for now

--- server/src/global/global.module.ts
+++ server/src/global/global.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GenerateUniquenameService } from './generate_uniquename/generate_uniquename.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [GenerateUniquenameService],
-  exports: [GenerateUniquenameService]
+  exports: [GenerateUniquenameService],
 })
 export class GlobalModule {}
