b65f028cc98a0457ba7727deb74c84fb61e8bbb0
Boburov Shukurullo <boburovdev@gmail.com>
2025-09-17 23:47:35 +0500

    fixed

--- server/src/playlists/lessons/lessons.module.ts
+++ server/src/playlists/lessons/lessons.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { LessonsService } from './lessons.service';
 import { LessonsController } from './lessons.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
-<<<<<<< HEAD
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
@@ -13,12 +12,6 @@ import { JwtModule } from '@nestjs/jwt';
       signOptions: { expiresIn: '7d' },
     }),
   ],
-=======
-import { AuthModule } from 'src/auth/auth.module';
-
-@Module({
-  imports: [PrismaModule,AuthModule],
->>>>>>> 4131f0e (fixed)
   controllers: [LessonsController],
   providers: [LessonsService],
 })
