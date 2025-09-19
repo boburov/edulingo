66e1d9f054a00cfc4e54ff6a7f7a6b0682338826
silvante <khamidov.ko@gmail.com>
2025-09-19 20:24:38 +0500

    deleting is ready

--- admin/.next/server/chunks/ssr/[root-of-the-server]__52f8ce16._.js
+++ admin/.next/server/chunks/ssr/[root-of-the-server]__52f8ce16._.js
@@ -136,7 +136,9 @@ const apiEndpoints = {
     getPlaylistByName: (unique_name)=>`/playlists/${unique_name}`,
     getAllPlaylists: "/playlists",
     // lessons
-    createNewLesson: (unique_name)=>`/playlists/${unique_name}/lessons/new`
+    createNewLesson: (unique_name)=>`/playlists/${unique_name}/lessons/new`,
+    updateLesson: (unique_name, id)=>`/playlists/${unique_name}/lessons/${id}`,
+    deleteLesson: (unique_name, id)=>`/playlists/${unique_name}/lessons/${id}`
 };
 const __TURBOPACK__default__export__ = apiEndpoints;
 }}),
