42c391e2fe0b867383e0af2d215f14a549e6603a
Boburov Shukurullo <boburovdev@gmail.com>
2025-09-22 20:48:56 +0500

    fixed

--- client/app/dashboard/[userId]/page.tsx
+++ client/app/dashboard/[userId]/page.tsx
@@ -6,25 +6,7 @@ import { useEffect } from "react";
 import Header from "../components/Header";
 
 const page = () => {
-  const routser = useRouter();
 
-  useEffect(() => {
-    const acces_token = localStorage.getItem("acces_token");
-    const refresh_token = localStorage.getItem("refresh_token");
-
-    if ((acces_token && refresh_token) !== null || undefined) {
-      const isUser = auth_service.verify(String(acces_token)).then((e) => {
-        return e.status;
-      });
-      console.log("token tasdiqlandi");
-    } else {
-      console.log("ha bu tokenlar null yoki undefined bo'lishi mumkun");
-    }
-
-    // if (isUser) {
-    //   routser.push("/auth/signin");
-    // }
-  }, []);
 
   return (
     <div>
