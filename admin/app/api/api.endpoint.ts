8463b5782f2328d1021dec52ea4c6341bf6d46e8
silvante <khamidov.ko@gmail.com>
2025-09-23 00:20:35 +0500

    playlists and lessons are fully ready

--- admin/app/api/api.endpoint.ts
+++ admin/app/api/api.endpoint.ts
@@ -10,9 +10,18 @@ const apiEndpoints = {
   getAllPlaylists: "/playlists",
 
   // lessons
-  createNewLesson: (unique_name: string) => `/playlists/${unique_name}/lessons/new`,
-  updateLesson: (unique_name: string, id: string) => `/playlists/${unique_name}/lessons/${id}`,
-  deleteLesson: (unique_name: string, id: string) => `/playlists/${unique_name}/lessons/${id}`
+  createNewLesson: (unique_name: string) =>
+    `/playlists/${unique_name}/lessons/new`,
+  updateLesson: (unique_name: string, id: string) =>
+    `/playlists/${unique_name}/lessons/${id}`,
+  deleteLesson: (unique_name: string, id: string) =>
+    `/playlists/${unique_name}/lessons/${id}`,
+
+  // vocs
+  getAllVocs: (lesson_id: string) => `/playlists/lessons/${lesson_id}/vocs/all`,
+  createVoc: (lesson_id: string) => `/playlists/lessons/${lesson_id}/vocs/new`,
+  deleteVoc: (lesson_id: string, id: string) =>
+    `/playlists/lessons/${lesson_id}/vocs/${id}/remove`,
 };
 
 export default apiEndpoints;
