635040375d2f0ef4c57a122cf0ea96db810e9a0f
Boburov Shukurullo <boburovdev@gmail.com>
2025-09-23 21:05:20 +0500

    Remove .next from repository

--- admin/.next/static/chunks/app_(root)_(panel)_lessons_[unique_name]_remove_page_tsx_ae53188a._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_(root)_(panel)_lessons_[unique_name]_remove_page_tsx_e29f4993._.js"
-],
-    source: "dynamic"
-});
--- admin/.next/static/chunks/app_(root)_(panel)_lessons_[unique_name]_update_page_tsx_ae53188a._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_(root)_(panel)_lessons_[unique_name]_update_7471dbf9._.js"
-],
-    source: "dynamic"
-});
